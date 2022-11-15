585738a4c9ce68025c5fe91ffd22022d770628b2
Yulei Liu <yulei.liu@gmail.com>
2022-11-16 10:21:32 +1100

    enable module

--- ent-aws/module_instance1.tf
+++ ent-aws/module_instance1.tf
@@ -1,10 +1,10 @@
-# //--------------------------------------------------------------------
-# // Modules
-# module "demo_ec2" {
-#   source  = "app.terraform.io/yulei/demo-ec2/aws"
-#   version = "0.0.2"
+//--------------------------------------------------------------------
+// Modules
+module "demo_ec2" {
+  source  = "app.terraform.io/yulei/demo-ec2/aws"
+  version = "0.0.2"
 
-#   ami_id = "ami-0caee806a0c3782c4"
-#   instance_name = "test_instance"
-#   key_name = "yulei-demo-key-oss-local"
-# }
+  ami_id = "ami-0caee806a0c3782c4"
+  instance_name = "test_instance"
+  key_name = "yulei-demo-key-oss-local"
+}
